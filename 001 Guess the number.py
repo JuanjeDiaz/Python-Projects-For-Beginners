9098bb5d79efec798ed8689176f5b5dd97d99a3e
Juanje Díaz <40724545+JuanjeDiaz@users.noreply.github.com>
2019-12-15 00:51:27 +0100

    Update 001 Guess the number.py

--- 001 Guess the number.py
+++ 001 Guess the number.py
@@ -6,10 +6,27 @@ print("Hi! let's play a game, I will think about one number between 0 and 20 and
 #Empieza el juego
 while True:
 	Number=randint(0,20)
-	guess=int(input("\nIn which number I'm thinking about?: "))
+	while True: 
+		try: guess=int(input("\nIn which number I'm thinking about?: "))
+		except ValueError: 
+			print("Sorry, enter a valid number.") 
+			continue
+		else: break
 	while(Number!=guess):
-		if(guess<Number): guess=int(input("Too low! Try again: "))
-		else: guess=int(input("Too high! Try again: "))
+		if(guess<Number): 
+			while True: 
+				try: guess=int(input("Too low! Try again: "))
+				except ValueError: 
+					print("Sorry, enter a valid number.") 
+					continue
+				else: break
+		else: 
+			while True: 
+				try: guess=int(input("Too high! Try again: "))
+				except ValueError: 
+					print("Sorry, enter a valid number.") 
+					continue
+				else: break
 	replay=0
 	while(replay!='Y' and replay!='N'):
 		replay=input("\nYou got it!! The number was "+ str(Number) +". Do you want to try again? (y/n): ").upper()
